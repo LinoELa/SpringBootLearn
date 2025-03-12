4bba9d2558f839e5aba3bf5d48d0a0aba46e6db5
Ela Lino <jamelarry22@gmail.com>
2025-03-12 08:35:57 +0100

    first commit

---
+++ src/main/java/es/pildoras/proyecto1/Proyecto1/rest/miRestController.java
@@ -0,0 +1,23 @@
+package es.pildoras.proyecto1.Proyecto1.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+
+// 1 V4
+//@RestController
+
+// 2 V5
+@Controller
+public class miRestController {
+	
+	// 2 V4
+	@GetMapping("/")
+	public String saludo() {
+		
+		return "index"; 
+	}
+	
+
+}
