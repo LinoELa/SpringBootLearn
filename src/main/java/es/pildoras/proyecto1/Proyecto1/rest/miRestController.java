4c893c956ec580d96d2310c78c6a25e3a9ac1db8
Ela Lino <jamelarry22@gmail.com>
2025-03-16 06:51:18 +0100

    Inyeccion de propiedades personalizadas : Configurar parrametros - Gestion de Valores (Value) - Ajustes de rendimiento - Control de dependencias de servicios externos.

--- src/main/java/es/pildoras/proyecto1/Proyecto1/rest/miRestController.java
+++ src/main/java/es/pildoras/proyecto1/Proyecto1/rest/miRestController.java
@@ -1,17 +1,42 @@
 package es.pildoras.proyecto1.Proyecto1.rest;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 
 // 1 V4
-//@RestController
+@RestController
 
 // 2 V5
-@Controller
+//@Controller
 public class miRestController {
+	
+	
+	// VALUE : Se usa pra inyectar valores a propiedades
+	
+	@Value("${ceo.pildoras.nombre}")
+	private String nombreCEO; 
+	
+	
+	@Value("${url.pildoras}")
+	private String urlPildoras; 
+	
+	
+	// Metodo que nos muestra los valores (Value) inyectados
+	
+	
+	@GetMapping("/datoscorporativos")
+	public String getDatosCorporativos() {
+		return "Nombre del CEO: " + nombreCEO + "<br>" + "Url de la web : " + urlPildoras; 
+	}
+	
+	
+	
+	
+	
 	
 	// 2 V4
 	@GetMapping("/")
